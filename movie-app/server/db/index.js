2084788d7f35e4260f974bfdc46065c221566382
sangravee <62633734+sangravee@users.noreply.github.com>
2020-03-25 23:28:31 +0700

    Update index.js

--- movie-app/server/db/index.js
+++ movie-app/server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb+srv://ppimper:groFHKrabbit4712@cluster0-drxuw.azure.mongodb.net/cinema?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', { useNewUrlParser: true })
+    .connect('mongodb+srv://ppimper:30604@cluster0-drxuw.azure.mongodb.net/cinema?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
